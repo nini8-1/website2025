d975073a993f103ebac7a5c1f5ca9a30a8a46596
nini8-1 <yixuancy3@gmail.com>
2025-11-05 15:12:51 +0800

    1105

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var server = express();
+var bodyParser = require("body-parser");
 
+server.use(express.static(__dirname+"/Public")); 
+server.use(bodyParser.urlencoded());
 
 server.get("/", (req,res)=>{
     res.send("Hello world!");
