d00374074bf5526f819def91bf3c9cd26429b20b
nini8-1 <yixuancy3@gmail.com>
2026-01-07 15:03:10 +0800

    0107

---
+++ FINAL/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
